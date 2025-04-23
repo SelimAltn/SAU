19d33f149f3a0e88bab175d8c7c3fe86e77f3959
sla55m <selim.altin@ogr.sakarya.edu.tr>
2025-04-23 21:43:34 +0300

    Parsing işlemleri ayrıştırıldı, her veri türü için parse metodları tanımlandı

--- src/io/DosyaOkuma.java
+++ src/io/DosyaOkuma.java
@@ -7,42 +7,47 @@ import java.util.ArrayList;
 import model.UzayAraci;
 
 public class DosyaOkuma {
-	public static ArrayList<Kisi> kisileriOku(String dosyaAdi) {
-		ArrayList<Kisi> kisiler = new ArrayList<>();
 
-		try (BufferedReader br = new BufferedReader(new FileReader(dosyaAdi))) {
-			String satir = br.readLine(); // BAŞLIĞI OKU VE ATLAMA YAP
-			
-
-			while ((satir = br.readLine()) != null) {
-			    String[] parca = satir.split("#");
-			    if (parca.length == 4) {
-			        String isim = parca[0];
-			        int yas = Integer.parseInt(parca[1]);
-			        int kalan = Integer.parseInt(parca[2]);
-			        String uzayAraci = parca[3];
-			        kisiler.add(new Kisi(isim, yas, kalan, uzayAraci));
-			    }
-			}
-		} catch (IOException e) {
-			System.out.println("Kisiler dosyasi okunurken hata oldu: " + e.getMessage());
-		}
-
-		return kisiler;
+	public static Kisi parseKisi(String satir) {
+		String[] parca = satir.split("#");
+		String isim = parca[0];
+		int yas = Integer.parseInt(parca[1]);
+		int kalan = Integer.parseInt(parca[2]);
+		String uzayAraci = parca[3];
+		return new Kisi(isim, yas, kalan, uzayAraci);
 	}
-	public static ArrayList<Gezegen> gezegenleriOku(String dosyaAdi) {
-	    ArrayList<Gezegen> gezegenler = new ArrayList<>();
+	
+
+	public static ArrayList<Kisi> kisileriOku(String dosyaAdi) {
+	    ArrayList<Kisi> kisiler = new ArrayList<>();
 
 	    try (BufferedReader br = new BufferedReader(new FileReader(dosyaAdi))) {
 	        String satir = br.readLine(); // başlık varsa atla
 	        while ((satir = br.readLine()) != null) {
-	            String[] parca = satir.split("#");
-	            if (parca.length == 3) {
-	                String ad = parca[0];
-	                int gunSaat = Integer.parseInt(parca[1]);
-	                String tarih = parca[2];
-	                gezegenler.add(new Gezegen(ad, gunSaat, tarih));
-	            }
+	            kisiler.add(parseKisi(satir));
+	        }
+	    } catch (IOException e) {
+	        System.out.println("Kisiler dosyası okunamadı: " + e.getMessage());
+	    }
+
+	    return kisiler;
+	}
+
+	public static Gezegen parseGezegen(String satir) {
+	    String[] parca = satir.split("#");
+	    String ad = parca[0];
+	    int gunSaat = Integer.parseInt(parca[1]);
+	    String tarih = parca[2];
+	    return new Gezegen(ad, gunSaat, tarih);
+	}
+
+	public static ArrayList<Gezegen> gezegenleriOku(String dosyaAdi) {
+	    ArrayList<Gezegen> gezegenler = new ArrayList<>();
+
+	    try (BufferedReader br = new BufferedReader(new FileReader(dosyaAdi))) {
+	        String satir = br.readLine();
+	        while ((satir = br.readLine()) != null) {
+	            gezegenler.add(parseGezegen(satir));
 	        }
 	    } catch (IOException e) {
 	        System.out.println("Gezegenler dosyası okunamadı: " + e.getMessage());
@@ -50,21 +55,25 @@ public class DosyaOkuma {
 
 	    return gezegenler;
 	}
+
+	
+	public static UzayAraci parseUzayAraci(String satir) {
+	    String[] parca = satir.split("#");
+	    String ad = parca[0];
+	    String cikis = parca[1];
+	    String varis = parca[2];
+	    String tarih = parca[3];
+	    int mesafe = Integer.parseInt(parca[4]);
+	    return new UzayAraci(ad, cikis, varis, tarih, mesafe);
+	}
+
 	public static ArrayList<UzayAraci> araclariOku(String dosyaAdi) {
 	    ArrayList<UzayAraci> araclar = new ArrayList<>();
 
 	    try (BufferedReader br = new BufferedReader(new FileReader(dosyaAdi))) {
-	        String satir = br.readLine(); // başlık varsa atla
+	        String satir = br.readLine();
 	        while ((satir = br.readLine()) != null) {
-	            String[] parca = satir.split("#");
-	            if (parca.length == 5) {
-	                String ad = parca[0];
-	                String cikis = parca[1];
-	                String varis = parca[2];
-	                String tarih = parca[3];
-	                int mesafe = Integer.parseInt(parca[4]);
-	                araclar.add(new UzayAraci(ad, cikis, varis, tarih, mesafe));
-	            }
+	            araclar.add(parseUzayAraci(satir));
 	        }
 	    } catch (IOException e) {
 	        System.out.println("Araclar dosyası okunamadı: " + e.getMessage());
@@ -72,6 +81,6 @@ public class DosyaOkuma {
 
 	    return araclar;
 	}
-	
+
 
 }
