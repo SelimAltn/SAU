7ce520e57172966051639ea083e3005730ac91e5
sla55m <selim.altin@ogr.sakarya.edu.tr>
2025-04-24 13:12:06 +0300

    Simülasyon saat döngüsü kuruldu, gezegen zamanları bağımsız olarak ilerletiliyor

--- src/model/Zaman.java
+++ src/model/Zaman.java
@@ -1,5 +1,47 @@
 package model;
 
 public class Zaman {
+	private int gun;
+    private int ay;
+    private int yil;
+    private int saat;
+    private int gunSaat; // 1 gün kaç saatten oluşuyor
+
+    public Zaman(String tarih, int gunSaat) {
+        String[] parca = tarih.split("\\.");
+        this.gun = Integer.parseInt(parca[0]);
+        this.ay = Integer.parseInt(parca[1]);
+        this.yil = Integer.parseInt(parca[2]);
+        this.saat = 0;
+        this.gunSaat = gunSaat;
+    }
+
+    public void ilerlet(int saatAdet) {
+        saat += saatAdet;
+        while (saat >= gunSaat) {
+            saat -= gunSaat;
+            gun++;
+            if (gun > 30) { // her ay 30 gün sayılır
+                gun = 1;
+                ay++;
+                if (ay > 12) {
+                    ay = 1;
+                    yil++;
+                }
+            }
+        }
+    }
+
+    public String tarihYaz() {
+        return gun + "." + ay + "." + yil + " - " + saat + ":00";
+    }
+
+    public int getSaat() {
+        return saat;
+    }
+
+    public String getTarihSadece() {
+        return gun + "." + ay + "." + yil;
+    }
 
 }
--- src/sim/Simulasyon.java
+++ src/sim/Simulasyon.java
@@ -1,28 +1,52 @@
 package sim;
 import io.DosyaOkuma;
+import java.util.*;
 import model.Kisi;
 import model.Gezegen;
 import model.UzayAraci;
+import model.Zaman;
 import java.util.ArrayList;
 
 public class Simulasyon {
 	public static void main(String[] args) {
-		 ArrayList<Kisi> kisiler = DosyaOkuma.kisileriOku("Kisiler.txt");
+		ArrayList<Kisi> kisiler = DosyaOkuma.kisileriOku("Kisiler.txt");
+        ArrayList<Gezegen> gezegenler = DosyaOkuma.gezegenleriOku("Gezegenler.txt");
+        ArrayList<UzayAraci> araclar = DosyaOkuma.araclariOku("Araclar.txt");
 
-	        for (Kisi k : kisiler) {
-	            System.out.println(k);
-	        }
-	        ArrayList<Gezegen> gezegenler = DosyaOkuma.gezegenleriOku("Gezegenler.txt");
+        HashMap<String, Zaman> gezegenZamanlari = new HashMap<>();
 
-	        for (Gezegen g : gezegenler) {
-	            System.out.println(g);
-	        }
-	        ArrayList<UzayAraci> araclar = DosyaOkuma.araclariOku("Araclar.txt");
+        for (Gezegen g : gezegenler) {
+            gezegenZamanlari.put(g.getAd(), new Zaman(g.getTarih(), g.getGunSaat()));
+        }
 
-	        for (UzayAraci a : araclar) {
-	            System.out.println(a);
-	        }
+        while (true) {
 
+            // Zamanı ilerlet
+            for (Zaman z : gezegenZamanlari.values()) {
+                z.ilerlet(1);
+            }
+
+            // Ekranı temizle
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            // Konsola güncel zamanları yaz
+            for (Map.Entry<String, Zaman> entry : gezegenZamanlari.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue().tarihYaz());
+            }
+
+            // TODO: Araç hareketleri, ömür düşürme, ölü kontrolü, bitiş kontrolü buraya eklenecek
+
+            try {
+                Thread.sleep(200); // simülasyon bekleme süresi (200ms)
+            } catch (InterruptedException e) {
+                break;
+            }
+
+        }
+		
+		
+	
+		
 	}
-
 }
